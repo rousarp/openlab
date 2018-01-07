f04f7c8d24aee83efb7c92aee8e73ac88acba998
Pavel Roušar <rousar.pavel@gmail.com>
2018-01-07 23:03:40 +0100

    úprava překladu

--- themes/openlab/lib/breadcrumbs.php
+++ themes/openlab/lib/breadcrumbs.php
@@ -17,7 +17,7 @@ add_action('bp_before_footer', 'openlab_do_breadcrumbs', 5);
 add_filter('openlab_breadcrumb_args', 'custom_breadcrumb_args');
 
 function custom_breadcrumb_args($args) {
-    $args['labels']['prefix'] = '<div class="breadcrumb-inline prefix-label"><div class="breadcrumb-prefix-label">You are here</div><i class="fa fa-caret-right"></i></div><div class="breadcrumb-inline breadcrumbs">';
+    $args['labels']['prefix'] = '<div class="breadcrumb-inline prefix-label"><div class="breadcrumb-prefix-label">Jste zde</div><i class="fa fa-caret-right"></i></div><div class="breadcrumb-inline breadcrumbs">';
     $args['prefix'] = '<div id="breadcrumb-container"><div class="breadcrumb-col semibold uppercase"><div class="breadcrumb-wrapper">';
     $args['suffix'] = '</div></div></div></div>';
     return $args;
